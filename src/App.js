7f7835f1ecb975ebf3c083c7c026c17385a6ffab
Oskar Risberg <oskar.risberg@stendahls.se>
2019-03-29 16:14:24 +0100

    Update with new unicator host

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 import loading from './loading.gif';
 
 // const UNICATOR_HOST = 'localhost:4000';
-const UNICATOR_HOST = 'https://unicator.stendahls.se';
+const UNICATOR_HOST = 'https://unicator.int.stendahls.dev';
 
 
 class App extends Component {
